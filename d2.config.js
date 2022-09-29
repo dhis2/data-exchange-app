6a56e31d735232c26bd88daf748076bba9d81560
Thomas Zemp <tom@dhis2.org>
2022-09-29 09:31:39 +0200

    chore: set up app platform

---
+++ d2.config.js
@@ -0,0 +1,11 @@
+const config = {
+    type: 'app',
+    name: 'aggregate-data-exchange',
+    title: 'Data Exchange',
+    minDHIS2Version: '2.39',
+    entryPoints: {
+        app: './src/App.js',
+    },
+}
+
+module.exports = config
