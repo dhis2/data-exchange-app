677e7076b6bae146b2bea07dd024079580fda0c6
Thomas Zemp <tom@dhis2.org>
2024-02-01 14:21:46 +0100

    fix: limit data items selector

--- src/components/edit/request-update/data-item-select.js
+++ src/components/edit/request-update/data-item-select.js
@@ -1,16 +1,23 @@
-import { DataDimension } from '@dhis2/analytics'
+import {
+    DataDimension,
+    dataTypeMap,
+    DIMENSION_TYPE_EXPRESSION_DIMENSION_ITEM,
+} from '@dhis2/analytics'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { SelectorValidationError } from '../shared/index.js'
 
 export const DataItemSelect = ({ input, meta }) => {
     const { value: selectedDimensions, onChange } = input
+    const enabledDataTypes = Object.values(dataTypeMap).filter(
+        ({ id }) => id !== DIMENSION_TYPE_EXPRESSION_DIMENSION_ITEM
+    )
 
     return (
         <>
             <DataDimension
+                enabledDataTypes={enabledDataTypes}
                 onSelect={({ items }) => {
-                    console.log(items)
                     onChange(items)
                 }}
                 selectedDimensions={selectedDimensions ?? []}
