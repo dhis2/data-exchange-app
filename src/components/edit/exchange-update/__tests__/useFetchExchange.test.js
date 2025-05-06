0cc12ec395e14f0eeab12d3ec0f629ede90a2ae6
Thomas Zemp <tom@dhis2.org>
2025-05-06 13:08:47 +0200

    fix: test formatting

--- src/components/edit/exchange-update/__tests__/useFetchExchange.test.js
+++ src/components/edit/exchange-update/__tests__/useFetchExchange.test.js
@@ -83,115 +83,103 @@ describe('getMetadataByRequest', () => {
             },
         ]
         expect(result).toEqual(EXPECTED)
-    }),
-        it('chunks ou items', () => {
-            const MOCK_VARIABLES = {
-                dx: ['dx_01', 'dx_02', 'dx_03', 'dx_04'],
-                pe: ['pe_01', 'pe_02'],
-                ou: [
-                    'ou_01',
-                    'ou_02',
-                    'ou_03',
-                    'ou_04',
-                    'ou_05',
-                    'ou_06',
-                    'ou_07',
-                    'ou_08',
-                    'ou_09',
-                    'ou_10',
-                    'ou_11',
-                    'ou_12',
-                ],
-            }
+    })
+    it('chunks ou items', () => {
+        const MOCK_VARIABLES = {
+            dx: ['dx_01', 'dx_02', 'dx_03', 'dx_04'],
+            pe: ['pe_01', 'pe_02'],
+            ou: [
+                'ou_01',
+                'ou_02',
+                'ou_03',
+                'ou_04',
+                'ou_05',
+                'ou_06',
+                'ou_07',
+                'ou_08',
+                'ou_09',
+                'ou_10',
+                'ou_11',
+                'ou_12',
+            ],
+        }
 
-            const result = getMetadataByRequest({
-                engine: MOCK_ENGINE,
-                variables: MOCK_VARIABLES,
-                index: 1,
-                chunkSize: 5,
-            })
-
-            const EXPECTED = [
-                {
-                    request: {
-                        query: 'query_name',
-                        variables: {
-                            variables: {
-                                dx: ['dx_01'],
-                                ou: [
-                                    'ou_01',
-                                    'ou_02',
-                                    'ou_03',
-                                    'ou_04',
-                                    'ou_05',
-                                ],
-                                pe: ['pe_01'],
-                            },
-                        },
-                    },
-                    index: 1,
-                },
-                {
-                    request: {
-                        query: 'query_name',
-                        variables: {
-                            variables: {
-                                dx: ['dx_01'],
-                                ou: [
-                                    'ou_06',
-                                    'ou_07',
-                                    'ou_08',
-                                    'ou_09',
-                                    'ou_10',
-                                ],
-                                pe: ['pe_01'],
-                            },
-                        },
-                    },
-                    index: 1,
-                },
-                {
-                    request: {
-                        query: 'query_name',
-                        variables: {
-                            variables: {
-                                dx: ['dx_01'],
-                                ou: ['ou_11', 'ou_12'],
-                                pe: ['pe_01'],
-                            },
-                        },
-                    },
-                    index: 1,
-                },
-                {
-                    request: {
-                        query: 'query_name',
-                        variables: {
-                            variables: {
-                                dx: ['dx_01', 'dx_02', 'dx_03', 'dx_04'],
-                                ou: ['ou_01'],
-                                pe: ['pe_01'],
-                            },
-                        },
-                    },
-                    index: 1,
-                },
-                {
-                    request: {
-                        query: 'query_name',
-                        variables: {
-                            variables: {
-                                dx: ['dx_01'],
-                                ou: ['ou_01'],
-                                pe: ['pe_01', 'pe_02'],
-                            },
-                        },
-                    },
-                    index: 1,
-                },
-            ]
-            expect(result).toEqual(EXPECTED)
+        const result = getMetadataByRequest({
+            engine: MOCK_ENGINE,
+            variables: MOCK_VARIABLES,
+            index: 1,
+            chunkSize: 5,
         })
+
+        const EXPECTED = [
+            {
+                request: {
+                    query: 'query_name',
+                    variables: {
+                        variables: {
+                            dx: ['dx_01'],
+                            ou: ['ou_01', 'ou_02', 'ou_03', 'ou_04', 'ou_05'],
+                            pe: ['pe_01'],
+                        },
+                    },
+                },
+                index: 1,
+            },
+            {
+                request: {
+                    query: 'query_name',
+                    variables: {
+                        variables: {
+                            dx: ['dx_01'],
+                            ou: ['ou_06', 'ou_07', 'ou_08', 'ou_09', 'ou_10'],
+                            pe: ['pe_01'],
+                        },
+                    },
+                },
+                index: 1,
+            },
+            {
+                request: {
+                    query: 'query_name',
+                    variables: {
+                        variables: {
+                            dx: ['dx_01'],
+                            ou: ['ou_11', 'ou_12'],
+                            pe: ['pe_01'],
+                        },
+                    },
+                },
+                index: 1,
+            },
+            {
+                request: {
+                    query: 'query_name',
+                    variables: {
+                        variables: {
+                            dx: ['dx_01', 'dx_02', 'dx_03', 'dx_04'],
+                            ou: ['ou_01'],
+                            pe: ['pe_01'],
+                        },
+                    },
+                },
+                index: 1,
+            },
+            {
+                request: {
+                    query: 'query_name',
+                    variables: {
+                        variables: {
+                            dx: ['dx_01'],
+                            ou: ['ou_01'],
+                            pe: ['pe_01', 'pe_02'],
+                        },
+                    },
+                },
+                index: 1,
+            },
+        ]
+        expect(result).toEqual(EXPECTED)
+    })
     it('chunks mix of items', () => {
         const MOCK_VARIABLES = {
             dx: ['dx_01', 'dx_02', 'dx_03', 'dx_04', 'dx_05', 'dx_06', 'dx_07'],
